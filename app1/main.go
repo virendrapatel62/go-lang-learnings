8abf73e30463a36bb47af2bd4a5fb74379e585bb
virendra-koloapp <119936949+virendra-koloapp@users.noreply.github.com>
2024-03-28 08:55:49 +0530

    Update main.go

--- app1/main.go
+++ app1/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func printNewLine() {
 	fmt.Println()
@@ -8,29 +11,64 @@ func printNewLine() {
 
 const ADD_TODO = 1
 const MARK_DONE = 2
+const PRINT_TODOS = 3
+const EXIT = 9
+
+var todos []string = []string{"Do somthing", "more work"}
 
 func printOptions() {
 	fmt.Println("Select an option please 👇")
 	fmt.Println("👉 1. To add todo")
 	fmt.Println("👉 2. To Mark a todo done")
+	fmt.Println("👉 3. To print all todos")
+	fmt.Println("👉 9. EXIT ❌")
 }
 
-func takeUserInput() int {
-	var optionInput int = 0
-	fmt.Scan(&optionInput)
-	return optionInput
+func takeUserInput() any {
+	var input any
+	fmt.Scan(&input)
+	return input
 }
 
 func performTask(selectedOption int) {
 	switch selectedOption {
 	case ADD_TODO:
 		{
+			fmt.Println("Todo title : (without any space please 😫) : ")
+			var todoToAdd string
+			fmt.Scan(&todoToAdd)
+			todos = append(todos, todoToAdd)
 
+			break
 		}
 
 	case MARK_DONE:
 		{
+			fmt.Println("Enter the sequence number of todo to mark done: ")
+			var serialNumber int
+			fmt.Scan(&serialNumber)
+			var index = serialNumber - 1
+			todos = append(todos[:index], todos[serialNumber:]...)
+			fmt.Println("Done ✅")
+			break
+		}
 
+	case PRINT_TODOS:
+		{
+			printNewLine()
+
+			fmt.Println("All the todos 🙌")
+
+			for index, value := range todos {
+				fmt.Printf("👉 %d. %s \n", index+1, value)
+			}
+			printNewLine()
+			break
+		}
+
+	case EXIT:
+		{
+			os.Exit(0)
 		}
 
 	}
@@ -40,8 +78,11 @@ func main() {
 	fmt.Println("Welcome to my first TODO app in GOLANG 🚀 ")
 	printNewLine()
 
-	printOptions()
-	var optionsInput int = takeUserInput()
-	performTask(optionsInput)
+	for true {
+		printOptions()
+		var option = 0
+		fmt.Scan(&option)
+		performTask((option))
+	}
 
 }
