cff7fa4d8c10a1d48d7827eab1bef74f8302cf48
virendra-koloapp <119936949+virendra-koloapp@users.noreply.github.com>
2024-03-30 13:15:20 +0530

    Added package example

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/calculator
+
+go 1.22.1
---
+++ utils.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func sum() {
+	fmt.Println("Sum Function called")
+}
+
+func substract() {
+	fmt.Println("Substract function called")
+}
