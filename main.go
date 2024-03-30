29cbd75bb80cb086cd5bbf1a0966bf7ba3155ce2
virendra-koloapp <patelvirendra62@gmail.com>
2024-03-30 14:37:01 +0530

    Added struct

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+type User struct {
+	firstName string
+	lastName  string
+	age       int
+	createAt  time.Time
+}
+
 func main() {
+
+	appUser := User{
+		firstName: "Virendra",
+		lastName:  "patel",
+		age:       24,
+		createAt:  time.Now(),
+	}
+
+	appUserCreatedByAnotherWay := User{"Virendra", "Patel", 25, time.Now()}
+
+	printUserValue(appUser)
+	printUserValue(appUserCreatedByAnotherWay)
+
+}
+
+func printUserValue(user User) {
+	fmt.Println("------------------------------")
+	fmt.Println("First Name : ", user.firstName)
+	fmt.Println("Last Name : ", user.lastName)
+	fmt.Println("Age : ", user.age)
+
+	fmt.Println("------------------------------")
+
+	fmt.Println()
 
 }
